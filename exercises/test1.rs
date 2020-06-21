fe350fe2af90d112a36c3876ce1576040a31d5cc
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-06-21 18:17:26 -0300

    Feito o teste 1.

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -7,17 +7,34 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
 // fn ..... {
 
 // Don't modify this function!
 #[test]
-fn verify_test() {
+fn verify_test()
+{
     let price1 = calculate_apple_price(35);
     let price2 = calculate_apple_price(65);
 
     assert_eq!(70, price1);
     assert_eq!(65, price2);
+}
+
+fn calculate_apple_price(quantidade: u32) -> u32
+{
+    let preco;
+
+    if(quantidade>40)
+    {
+        preco=quantidade;
+    }
+    else
+    {
+        preco=quantidade*2;
+    }
+
+    return preco;
 }
