1481e112a35d7ea9007a918a7c783744a82d4abe
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-06-21 11:49:20 -0300

    Foi feito o exercício 1 dos ifs.

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,8 +1,17 @@
 // if1.rs
 
-// I AM NOT DONE
+// I AM DONE
 
-pub fn bigger(a: i32, b: i32) -> i32 {
+pub fn bigger(a: i32, b: i32) -> i32
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
@@ -12,16 +21,19 @@ pub fn bigger(a: i32, b: i32) -> i32 {
 
 // Don't mind this for now :)
 #[cfg(test)]
-mod tests {
+mod tests
+{
     use super::*;
 
     #[test]
-    fn ten_is_bigger_than_eight() {
+    fn ten_is_bigger_than_eight()
+    {
         assert_eq!(10, bigger(10, 8));
     }
 
     #[test]
-    fn fortytwo_is_bigger_than_thirtytwo() {
+    fn fortytwo_is_bigger_than_thirtytwo()
+    {
         assert_eq!(42, bigger(32, 42));
     }
 }
