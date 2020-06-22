39cd9b89a00b4eb14792f968a00012dd88175dc2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-06-22 05:10:10 -0300

    Feito os exercícios de tipos primitivos

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,18 @@
 // Create an array with at least 100 elements in it where the ??? is. 
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
+// I AM DONE
 
-fn main() {
-    let a = ???
+fn main()
+{
+    let a = [0; 2000];
 
-    if a.len() >= 100 {
+    if a.len() >= 100
+    {
         println!("Wow, that's a big array!");
-    } else {
+    }
+    else
+    {
         println!("Meh, I eat arrays like that for breakfast.");
     }
 }
